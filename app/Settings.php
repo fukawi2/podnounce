7ac37d8739f66769347448320a9d8144f2e7c22b
Phillip Smith <fukawi2@gmail.com>
2018-10-01 14:51:09 +1000

    initial commit of toast and settings

---
+++ app/Settings.php
@@ -0,0 +1,52 @@
+<?php
+
+Class Settings extends Controller {
+
+  function Main($f3,$params) {
+    $db_setting = new DB\SQL\Mapper($f3->get('DB'), 'settings');
+
+    if ($f3->VERB == 'POST')
+      $this->__MainPOST($f3,$params);
+
+    // retrieve settings from database
+    foreach ($this->setting_keys as $key => $required) {
+      $db_setting->load(array('setting=?',$key));
+      $f3->set('SETTINGS.'.$key, trim($db_setting->value));
+    }
+
+    $f3->set('PAGE.TITLE', 'Settings');
+    $f3->set('PAGE.HEADER', 'Settings');
+    $f3->set('PAGE.CONTENT','settings.htm');
+    echo \Template::instance()->render('layouts/default.htm');
+  }
+
+
+
+  private function __MainPOST($f3,$params) {
+    $db_setting = new DB\SQL\Mapper($f3->get('DB'), 'settings');
+    $db_user = new DB\SQL\Mapper($f3->get('DB'), 'users');
+
+    // validate user input
+    foreach ($this->setting_keys as $key => $required) {
+      $value = $this->NullIfEmpty($f3->get('POST.'.$key));
+      if ($required and empty($value)) {
+        $f3->set('SESSION.TOAST.msg', $key.' is required');
+        $f3->set('SESSION.TOAST.class', 'error');
+        return false;
+      }
+    }
+
+    // save settings to the database
+    foreach ($this->setting_keys as $key => $required) {
+      $db_setting->load(array('setting=?',$key));
+      $value = $f3->get('POST.'.$key);
+      $db_setting->setting = $key;
+      $db_setting->value = $value;
+      $db_setting->save();
+      $db_setting->reset();
+    }
+
+    $f3->set('SESSION.TOAST.msg', 'Settings Updated.');
+    $f3->set('SESSION.TOAST.class', 'success');
+  }
+}
