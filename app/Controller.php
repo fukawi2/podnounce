53ca2765c6070c2edba9f7a4d102e95fa9c7d1d3
Phillip Smith <fukawi2@gmail.com>
2018-10-19 10:04:42 +1100

    bump version constant to 1.2

--- app/Controller.php
+++ app/Controller.php
@@ -2,7 +2,7 @@
 
 class Controller {
 
-  const VERSION = '1.1';
+  const VERSION = '1.2';
 
   /* general settings that are stored in the database are defined here for
    * batch processing in loops etc. the array key is the name of the setting
